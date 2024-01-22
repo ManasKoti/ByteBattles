00c950c8c997693109fd716d6b01a121c397c630
ManasKoti <manaskoti00@gmail.com>
2024-01-23 00:43:19 +0530

    final commit before pre-release

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ shard_no = 0
 game_on = True
 while game_on:
      
-    player_name = input("Welcome to Byte Battles!!\nWhat is your name adventurer your name?\n")
+    player_name = input("Welcome to Byte Battles!!\nWhat is your name adventurer?\n")
     print()
     print("Welcome " + player_name + "!\n")
     player = Player(player_name)
